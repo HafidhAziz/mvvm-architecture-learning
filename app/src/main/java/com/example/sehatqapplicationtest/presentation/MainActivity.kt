c1b85c0e79a663521d8ab8e970044105c14c8b31
Hafidh <m.hafidh@alodokter.com>
2021-01-21 23:26:48 +0700

    provide main activity and item adapter layout

--- app/src/main/java/com/example/sehatqapplicationtest/presentation/MainActivity.kt
+++ app/src/main/java/com/example/sehatqapplicationtest/presentation/MainActivity.kt
@@ -2,13 +2,45 @@ package com.example.sehatqapplicationtest.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.example.sehatqapplicationtest.R
+import com.example.sehatqapplicationtest.databinding.ActivityMainBinding
+import com.example.sehatqapplicationtest.presentation.home.HomeFragment
+import com.example.sehatqapplicationtest.presentation.purchasehistory.PurchaseHistoryFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.bnMain.setOnNavigationItemSelectedListener(this)
+
+        loadFragment(HomeFragment())
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+        when (item.itemId) {
+            R.id.home_menu -> fragment = HomeFragment()
+            R.id.profile_menu -> fragment = PurchaseHistoryFragment()
+        }
+        return loadFragment(fragment)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fl_container, fragment)
+                .commit()
+            return true
+        }
+        return false
     }
 }
